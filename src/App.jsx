d45275c1eb304124a5dca81e38876e352f7482ed
Umar033ya <umaryakubov033ppp@gmail.com>
2025-11-27 20:55:43 +0500

    header add

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
-
+import Header from './components/Header/Header.jsx'
 function App() {
-
+   const [coin, setCoin] = useState(0)
   return (
     <>
-     
+     <Header coin= {coin}/>
     </>
   )
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import './Header.css'
 import logo from '../../../public/DF.png'
-function Header() {
+
+function Header({coin}) {
   return (
     <header>
-
+        <div className="container">
+            <nav>
+                <div className="nav__left">
+                    <img src={logo} alt="logo" />
+                    <select>
+                        <option value="standoff2">Standoff 2</option>
+                        <option value="cs2">CS 2</option>
+                    </select>
+                </div>
+                <div className="nav__end">
+                    <div className="price_div">
+                        <p>{coin}</p>
+                        <i class="fa-solid fa-plus"></i>
+                    </div>
+                    <div className="avatar">
+                    <i class="fa-solid fa-circle-user"></i>
+                    </div>
+                </div>
+            </nav>
+        </div>
     </header>
   )
 }
