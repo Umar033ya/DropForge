bd6c51dd9c0204bdc6c934bab249e0c92ea4eed2
Umar033ya <umaryakubov033ppp@gmail.com>
2025-12-14 09:35:24 +0500

    swiper change

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -12,7 +12,7 @@ import case3 from '../../../public/case3.jpg'
 import swiper1 from '../../../public/SWIPER1.jpg'
 import swiper2 from '../../../public/swiper2.jpg'
 import swiper3 from '../../../public/swiper3.jpg'
-import swiper4 from '../../../public/swiper4.png'
+import swiper4 from '../../../public/swiper4.jpg'
 function Home({ data }) {
   return (
     <div>
@@ -64,12 +64,12 @@ function Home({ data }) {
         <h1 className="cases-title">Cases</h1>
         <div className="cases-container">
         <Cases caseImg={logo} caseName="Drop Forge"
-  skinIds={[1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58]}
-  price={30}
-/>
+        skinIds={[1, 4, 7, 10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58]}
+        price={5}
+        />
 <Cases caseImg={case1} caseName="Epic Vault"
   skinIds={[2, 5, 8, 11, 14, 17, 20, 23, 26, 29, 32, 35, 38, 41, 44, 47, 50, 53, 56, 59]}
-  price={50}
+  price={24}
 />
 <Cases caseImg={case2} caseName="Legendary Box"
   skinIds={[3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60]}
@@ -78,12 +78,12 @@ function Home({ data }) {
 
 <Cases caseImg={case3} caseName="Mystic Chest"
   skinIds={[4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80]}
-  price={28}
+  price={24}
 />
 
 <Cases caseImg={case1} caseName="Shadow Crate"
   skinIds={[5, 9, 13, 17, 21, 25, 29, 33, 37, 41, 45, 49, 53, 57, 61, 65, 69, 73, 77, 81]}
-  price={45}
+  price={22}
 />
 
 <Cases caseImg={logo} caseName="Crimson Fury"
@@ -93,17 +93,17 @@ function Home({ data }) {
 
 <Cases caseImg={case2} caseName="Emerald Strike"
   skinIds={[7, 11, 15, 19, 23, 27, 31, 35, 39, 43, 47, 51, 55, 59, 63, 67, 71, 75, 79, 83]}
-  price={40}
+  price={30}
 />
 
 <Cases caseImg={case3} caseName="Golden Phantom"
   skinIds={[1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39]}
-  price={55}
+  price={15}
 />
 
 <Cases caseImg={logo} caseName="Shadow Reaper"
   skinIds={[2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]}
-  price={30}
+  price={25}
 />
 
 <Cases caseImg={case3} caseName="Frostbite"
@@ -113,30 +113,28 @@ function Home({ data }) {
 
 <Cases caseImg={case2} caseName="Inferno Blaze"
   skinIds={[3, 6, 9, 12, 15, 18, 21, 24, 27, 30, 33, 36, 39, 42, 45, 48, 51, 54, 57, 60]}
-  price={50}
+  price={30}
 />
 
 <Cases caseImg={case1} caseName="Violet Vortex"
   skinIds={[4, 8, 12, 16, 20, 24, 28, 32, 36, 40, 44, 48, 52, 56, 60, 64, 68, 72, 76, 80]}
-  price={28}
+  price={25}
 />
 
 <Cases caseImg={logo} caseName="Obsidian Edge"
   skinIds={[5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 6, 12, 18, 24]}
-  price={65}
+  price={10}
 />
 
 <Cases caseImg={case2} caseName="Azure Phantom"
   skinIds={[7, 14, 21, 28, 35, 42, 49, 56, 63, 70, 77, 4, 11, 18, 25, 32, 39, 46, 53, 60]}
-  price={18}
+  price={15}
 />
 
 <Cases caseImg={case1} caseName="Crimson Reign"
   skinIds={[8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 9, 18, 27, 36, 45, 54, 63, 72, 3, 12]}
-  price={34}
+  price={30}
 />
-
-
         </div>
       </div>
     </div>
